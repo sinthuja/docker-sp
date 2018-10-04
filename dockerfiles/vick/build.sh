705ba80bb77a5cbee15a1f57b8565b2148454128
sinthuja <s.rajan.sinthu@gmail.com>
2018-10-04 11:06:15 +0530

    SP 4.3.0 deployment.

---
+++ dockerfiles/vick/build.sh
@@ -0,0 +1,6 @@
+kubectl delete ns vick-system
+cd dashboard
+docker build -t vick-wso2sp-dashboard:4.3.0 .
+cd ../worker
+docker build -t vick-wso2sp-worker:4.3.0 .
+kubectl apply -f ../../../kubernetes/mysql-deployment.yaml
